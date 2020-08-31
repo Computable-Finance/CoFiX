2d93a2a991a4284297c29a8c1c813d73fedfccc6
kazamio <kazamio@protonmail.com>
2020-08-31 12:33:03 +0800

    scripts/queryOracle: price format

--- scripts/queryOracle.js
+++ scripts/queryOracle.js
@@ -4,6 +4,7 @@ const { BN } = require('@openzeppelin/test-helpers');
 const { web3 } = require('@openzeppelin/test-environment');
 const CofiXController = artifacts.require("CofiXController");
 const CofiXPair = artifacts.require("CofiXPair");
+const Decimal = require('decimal.js');
 
 const argv = require('yargs').argv;
 
@@ -21,11 +22,9 @@ module.exports = async function (callback) {
         } else {
             PriceOracle = await NEST3PriceOracleMock.at(argv.oracle);
         }
-        if (argv.token === "" || argv.token === undefined) {
-            Token = await ERC20.deployed();
-        } else {
-            Token = await ERC20.at(argv.token);
-        }
+
+        Token = await ERC20.at(argv.token);
+
         // if (argv.pair === "" || argv.token === undefined) {
         //     Pair = await CofiXPair.deployed();
         // } else {
@@ -51,7 +50,14 @@ module.exports = async function (callback) {
         // get price now from NEST3PriceOracleMock Contract
         let p = await PriceOracle.checkPriceNow(Token.address);
         let decimal = await Token.decimals();
-        console.log(`price now> ethAmount=${p.ethAmount.toString()}, erc20Amount=${p.erc20Amount.toString()}, price=${p.erc20Amount.mul(new BN(web3.utils.toWei('1', 'ether'))).div(p.ethAmount).div((new BN('10')).pow(decimal)).toString()} ${symbol}/ETH`);
+
+        // p.erc20Amount.mul(new BN(web3.utils.toWei('1', 'ether'))).div(p.ethAmount).div((new BN('10')).pow(decimal)).toString()
+        const ethBase = Decimal((new BN(web3.utils.toWei('1', 'ether'))).toString());
+        const tokenDecimal = Decimal(((new BN('10')).pow(decimal)).toString());
+
+        let price = Decimal(p.erc20Amount.toString()).mul(ethBase).div(Decimal(p.ethAmount.toString())).div(tokenDecimal);
+        
+        console.log(`price now> ethAmount=${p.ethAmount.toString()}, erc20Amount=${p.erc20Amount.toString()}, price=${price} ${symbol}/ETH`);
 
         // get K_BASE from CofiXController contract
         let k_base = await CofiXCtrl.K_BASE(); // 100000

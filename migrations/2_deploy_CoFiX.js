e282a76156ee1105d23644e87c56a23c15680ec7
kazamio <kazamio@protonmail.com>
2020-09-07 17:34:34 +0800

    update deploy script

--- migrations/2_deploy_CoFiX.js
+++ migrations/2_deploy_CoFiX.js
@@ -1,13 +1,13 @@
-const USDT = artifacts.require("test/USDT");
-const HBTC = artifacts.require("test/HBTC");
-const NEST = artifacts.require("test/NEST");
-const WETH9 = artifacts.require("test/WETH9");
+var USDT = artifacts.require("test/USDT");
+var HBTC = artifacts.require("test/HBTC");
+var NEST = artifacts.require("test/NEST");
+var WETH9 = artifacts.require("test/WETH9");
 // const NEST3PriceOracleMock = artifacts.require("mock/NEST3PriceOracleMock");
-const NEST3PriceOracleMock = artifacts.require("mock/NEST3PriceOracleAutoUpdateConstMock");
+var NestPriceOracle = artifacts.require("mock/NEST3PriceOracleAutoUpdateConstMock");
+var CoFiXKTable = artifacts.require("CoFiXKTable");
 const CoFiXFactory = artifacts.require("CoFiXFactory");
-const CoFiXRouter = artifacts.require("CoFiXRouter");
-const CoFiXKTable = artifacts.require("CoFiXKTable");
 const CoFiXController = artifacts.require("CoFiXController");
+const CoFiXRouter = artifacts.require("CoFiXRouter");
 const { deployProxy } = require('@openzeppelin/truffle-upgrades');
 
 
@@ -25,14 +25,13 @@ module.exports = async function(deployer) {
     // WETH contract
     await deployer.deploy(WETH9);
 
+    // USDT = await USDT.at("0xD52d3bfCA0d39E4bD5378e0BBa8AD245C3F58C17");
+    // HBTC = await HBTC.at("0x9aA0AF152cf141740f19D335b5ddE1F0E51008A7");
+    // NEST = await NEST.at("0xB9746A8572DB5C27597fE88B86B6520599Bf62d4");
+    // WETH9 = await WETH9.at("0xB3d7C7993BE7bEec23D005552224B2dAf18Bd85E");
+
     // NEST3 Price Oracle Mock
-    await deployer.deploy(NEST3PriceOracleMock, NEST.address);
-
-    // let controller = await CoFiXController.deployed();
-    // await controller.initialize(NEST3PriceOracleMock.address);
-
-    // // Wait for NEST Oracle sending eth by `call` instead of `transfer`
-    // await deployProxy(CoFiXController, [NEST3PriceOracleMock.address], { deployer });
+    await deployer.deploy(NestPriceOracle, NEST.address);
 
     // CoFiXFactory
     await deployer.deploy(CoFiXFactory, WETH9.address);
@@ -40,7 +39,7 @@ module.exports = async function(deployer) {
     await deployer.deploy(CoFiXKTable);
 
     // CoFiXController
-    await deployer.deploy(CoFiXController, NEST3PriceOracleMock.address, NEST.address, CoFiXFactory.address, CoFiXKTable.address);
+    await deployer.deploy(CoFiXController, NestPriceOracle.address, NEST.address, CoFiXFactory.address, CoFiXKTable.address);
 
     // set controller in factory
     let factory = await CoFiXFactory.deployed();
@@ -53,8 +52,8 @@ module.exports = async function(deployer) {
     console.log(`| USDT | ${USDT.address} |`);
     console.log(`| HBTC | ${HBTC.address} |`);
     console.log(`| NEST | ${NEST.address} |`);
-    console.log(`| WETH9 | ${WETH9.address} |`);
-    console.log(`| NEST3PriceOracleMock | ${NEST3PriceOracleMock.address} |`);
+    console.log(`| WETH | ${WETH9.address} |`);
+    console.log(`| NestPriceOracle | ${NestPriceOracle.address} |`);
     console.log(`| CoFiXController | ${CoFiXController.address} |`);
     console.log(`| CoFiXFactory | ${CoFiXFactory.address} |`);
     console.log(`| CoFiXKTable | ${CoFiXKTable.address} |`);

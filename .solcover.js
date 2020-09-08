c0b02afd147c51637d969e7b0e56ad81dd9c421f
kazamio <kazamio@protonmail.com>
2020-09-09 00:46:09 +0800

    coverage config

--- .solcover.js
+++ .solcover.js
@@ -2,6 +2,8 @@ module.exports = {
     skipFiles: [
         'Migrations.sol',
         'test/',
-        'lib/ABDKMath64x64.sol'
+        'lib/ABDKMath64x64.sol',
+        'interface/',
+        'mock/NEST3PriceOracleAutoUpdateConstMock.sol'
       ],
 };

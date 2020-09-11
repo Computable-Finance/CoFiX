6b7b9585368ce51b08013c239a1080689ccf1179
kazamio <kazamio@protonmail.com>
2020-09-11 19:11:36 +0800

    coverage config

--- .solcover.js
+++ .solcover.js
@@ -2,6 +2,7 @@ module.exports = {
     skipFiles: [
         'Migrations.sol',
         'Timelock.sol',
+        'CoFiXERC20.sol',
         'test/',
         'lib/ABDKMath64x64.sol',
         'lib/TransferHelpers.sol',

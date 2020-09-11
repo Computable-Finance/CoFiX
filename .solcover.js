1bc55f5aa14c0b7143cfa4fb92b431bb352426e7
kazamio <kazamio@protonmail.com>
2020-09-11 17:35:15 +0800

    minor

--- .solcover.js
+++ .solcover.js
@@ -1,8 +1,11 @@
 module.exports = {
     skipFiles: [
         'Migrations.sol',
+        'Timelock.sol',
         'test/',
         'lib/ABDKMath64x64.sol',
+        'lib/TransferHelpers.sol',
+        'lib/SafeMath.sol',
         'interface/',
         'mock/NEST3PriceOracleAutoUpdateConstMock.sol'
       ],

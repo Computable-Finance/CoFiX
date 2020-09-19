8e8cb5ecc543fdfee23b777915e0f18b38704c9b
kazamio <kazamio@protonmail.com>
2020-09-19 12:09:41 +0800

    test: CoFiXStakingRewards stakeForOther

--- test/CoFiXStakingRewards.test.js
+++ test/CoFiXStakingRewards.test.js
@@ -105,7 +105,7 @@ contract('CoFiXStakingRewards', (accounts) => {
         expect(accrued).to.bignumber.equal(reward);
         expect(earned).to.bignumber.equal(reward);
         expect(rewardPerToken).to.bignumber.equal(reward);
-    });  
+    });
 
     it("should exit (withdraw & getReward) correctly", async () => {
         // stats before exit
@@ -132,4 +132,70 @@ contract('CoFiXStakingRewards', (accounts) => {
         }
     });
 
+    it("should stakeForOther (LP2) correctly", async () => {
+        const amount = web3.utils.toWei('1', 'ether');
+        await XToken.approve(StakingRewards.address, amount, {from: LP_1});
+        await StakingRewards.stakeForOther(LP_2, amount, {from: LP_1});
+    });
+
+    it("should have correct stats after stakeForOther (LP_2)", async () => {
+        const amount = web3.utils.toWei('1', 'ether');
+        const balanceInStake = await StakingRewards.balanceOf(LP_2);
+        expect(balanceInStake).to.bignumber.equal(amount);
+        const totalSupply = await StakingRewards.totalSupply();
+        expect(totalSupply).to.bignumber.equal(amount);
+
+        const balanceInStakeForLP1 = await StakingRewards.balanceOf(LP_1);
+        expect(balanceInStakeForLP1).to.bignumber.equal("0");
+
+        // calc accrued() & earned()
+        let accrued = await StakingRewards.accrued();
+        let earned = await StakingRewards.earned(LP_2);
+        let rewardPerToken = await StakingRewards.rewardPerToken();
+        let userRewardPerTokenPaidForLP2 = await StakingRewards.userRewardPerTokenPaid(LP_2);
+        expect(accrued).to.bignumber.equal("0");
+        expect(earned).to.bignumber.equal("0");
+        expect(rewardPerToken).to.bignumber.equal(userRewardPerTokenPaidForLP2);
+        // push the block
+        await time.advanceBlock();
+        accrued = await StakingRewards.accrued();
+        earned = await StakingRewards.earned(LP_2);
+        rewardPerToken = await StakingRewards.rewardPerToken();
+        if (verbose) {
+            console.log(`accrued: ${accrued}`);
+            console.log(`earned: ${earned}`);
+            console.log(`rewardPerToken: ${rewardPerToken}`);
+            console.log(`userRewardPerTokenPaidForLP2: ${userRewardPerTokenPaidForLP2}`);
+        }
+        let reward = new BN(INIT_COFI_RATE).mul(new BN(1));
+        expect(accrued).to.bignumber.equal(reward);
+        expect(earned).to.bignumber.equal(reward);
+        const expectedRewardPerToken = (new BN(userRewardPerTokenPaidForLP2)).add(new BN(reward));
+        expect(rewardPerToken).to.bignumber.equal(expectedRewardPerToken);
+    });
+
+    it("should exit (withdraw & getReward) correctly for LP2", async () => {
+        // stats before exit
+        let reward = await CoFi.balanceOf(LP_2);
+        expect(reward).to.bignumber.equal("0");
+        let balance = await XToken.balanceOf(LP_2);
+        expect(balance).to.bignumber.equal("0");
+        if (verbose) {
+            console.log(`before exit, CoFi reward Of LP_2: ${reward}`);
+            console.log(`before exit, XToken balance Of LP_2: ${balance}`);
+        }
+        // withdraw and getReward
+        await StakingRewards.exit({from: LP_2});
+        let expectedReward = new BN(INIT_COFI_RATE).mul(new BN(2)); // means reward in two blocks
+        // stats after exit
+        reward = await CoFi.balanceOf(LP_2);
+        expect(expectedReward).to.bignumber.equal(reward);
+        balance = await XToken.balanceOf(LP_2);
+        expectedBalance = web3.utils.toWei('1', 'ether');
+        expect(expectedBalance).to.bignumber.equal(balance);
+        if (verbose) {
+            console.log(`after exit, CoFi reward Of LP_2: ${reward}`);
+            console.log(`after exit, XToken balance Of LP_2: ${balance}`);
+        }
+    });
 });
